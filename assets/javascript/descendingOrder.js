6a0033c8f1c1d663eb0ccfa5eb1da14288a09517
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-09 16:40:15 +0000

    Added descendingOrder (not enetered in codewars)

---
+++ assets/javascript/descendingOrder.js
@@ -0,0 +1,28 @@
+/**
+|--------------------------------------------------
+| Descending Order
+| 
+| Your task is to make a function that can take any non-negative integer 
+| as a argument and return it with its digits in descending order.
+| 
+| Essentially, rearrange the digits to create the highest possible number.
+| 
+| Examples:
+| Input: 21445 Output: 54421
+| Input: 145263 Output: 654321
+| Input: 1254859723 Output: 9875543221
+|--------------------------------------------------
+*/
+
+const descendingOrder = num => Number(num.toString().split('').sort().reverse().map(string => parseInt(string)).join(''))
+
+const answer = descendingOrder(123456789) //?
+typeof(answer) //?
+
+/**
+|--------------------------------------------------
+| Test.assertEquals(descendingOrder(0), 0)
+| Test.assertEquals(descendingOrder(1), 1)
+| Test.assertEquals(descendingOrder(123456789), 987654321)
+|--------------------------------------------------
+*/
