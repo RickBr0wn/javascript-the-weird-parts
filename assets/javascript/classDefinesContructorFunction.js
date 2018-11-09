cd07969eb5706e9b9a47b82348abca4dfc18188a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-09 14:32:14 +0000

    Cleaned comment blocks

--- assets/javascript/classDefinesContructorFunction.js
+++ assets/javascript/classDefinesContructorFunction.js
@@ -1,11 +1,11 @@
-function makeClass() {
+// function makeClass() {
   class Vegetable {
     constructor(vegetable) {
       this.name = vegetable
     }
   }
-  return Vegetable
-}
+//   return Vegetable
+// }
 const Vegetable = makeClass()
 const carrot = new Vegetable('carrot')
 
--- assets/javascript/uniqueInOrder.js
+++ assets/javascript/uniqueInOrder.js
@@ -1,12 +1,13 @@
 /**
 |--------------------------------------------------
-| Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the    || original order of elements.
-
-  For example:
-
-  uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-  uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-  uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+| Implement the function unique_in_order which takes as argument a   
+| without any elements with the same value sequence and returns a 
+| list of itemsnext to each other and preserving the original order of elements.
+| 
+| For example:
+|  uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+|  uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+|  uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 |--------------------------------------------------
 */
 
