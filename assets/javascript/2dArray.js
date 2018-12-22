a19591888e8a9f17f8fa606f04a3183f50591284
RickBr0wn <ricky.brown.00@gmail.com>
2018-12-22 10:36:43 +0000

    Cleaned create2dArray type description

--- assets/javascript/2dArray.js
+++ assets/javascript/2dArray.js
@@ -1,7 +1,21 @@
-const create2dArray = (length) => {
+/**
+* create2dArray
+*
+* A function that returns a 2d Array, that can be visualized as a grid, with an X & Y axis
+* eg: create2dArray(3)
+* *  X, X, X,
+* *  X, X, X
+* *  X, X, X
+*
+* @param number - the size of the 'X' axis
+* @param number - the size of the 'Y' axis
+* @return array - an array that represents a grid 
+*
+*/
+ const create2dArray = (x, y) => {
   const arr = []
-  for(let i = 0; i < length; i++){
-    for(let j = 0; j < length; j++ ){
+  for(let i = 0; i < x; i++){
+    for(let j = 0; j < y; j++ ){
       arr[i][j].push('X')
     }
   }
--- assets/javascript/sumMulitpleArgs.js
+++ assets/javascript/sumMulitpleArgs.js
@@ -1,6 +1,5 @@
-const sumMultipleArgs = (function() {
-  return function sumMultipleArgs(...args) {
-    return args.reduce((a, b) => a + b, 0);
-  };
-})();
-console.log(sumMultipleArgs(1, 2, 3)); // 6
+function sumMultipleArgs(...args) {
+    return args.reduce((a, b) => a + b)
+  }
+
+console.log(sumMultipleArgs(1, 2, 3)) //?
