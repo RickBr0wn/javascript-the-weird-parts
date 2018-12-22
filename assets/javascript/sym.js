b156c0a4e197c60eea50f0bc767b09991ebce16b
RiCK BROWN <ricky.brown.00@gmail.com>
2018-12-22 00:12:08 +0000

    Renamed file, refactored and improved answer

---
+++ assets/javascript/sym.js
@@ -0,0 +1,24 @@
+function sym() { //?
+    return Array(...arguments).reduce(testForDifference) //?
+  }
+  
+  const testForDifference = (arrayOne, arrayTwo) => {
+    let answer = new Array()
+  
+    // .indexOf will return -1 if the item is NOT in the array, or its index (0 or over) if it is present
+    arrayOne.forEach(item => {
+      if(arrayTwo.indexOf(item) < 0 && answer.indexOf(item) < 0){
+          answer.push(item)
+      }
+    })
+  
+    arrayTwo.forEach(item => {
+      if(arrayOne.indexOf(item) < 0 && answer.indexOf(item) < 0){
+          answer.push(item)
+      }
+    })
+    return answer
+  }
+  
+  sym([1, 2, 3], [5, 2, 1, 4]) //?
+  
